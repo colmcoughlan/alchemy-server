31b7ad0eaf4f74503a970e0cee303eb3bc5ea460
Colm Coughlan <colmcoughlanirl@gmail.com>
2017-06-04 16:44:15 +0100

    Switch to datetime based on calls for updates.

--- charity_server.py
+++ charity_server.py
@@ -9,8 +9,10 @@ Created on Sun Apr 30 01:14:12 2017
 from flask import Flask, jsonify
 from parse_likecharity import refresh_charities
 import threading
+from datetime import datetime
 
 refresh_rate = 24 * 60 * 60 #Seconds
+start_time = datetime.now()
 
 # variables that are accessible from anywhere
 payload = {}
@@ -37,8 +39,12 @@ def update_charities():
   backgroundThread = threading.Timer(refresh_rate, update_charities, ())
   backgroundThread.start() 
 
-@app.route("/")
+@app.route("/gci")
 def gci():
+  delta = datetime.now() - start_time
+  if delta.total_seconds() > refresh_rate:
+      categories, charity_dict = refresh_charities()
+      payload = {'categories':categories, 'charities':charity_dict}
   return jsonify(payload)
 
 if __name__ == "__main__":
